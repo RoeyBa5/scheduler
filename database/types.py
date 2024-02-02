478ae0ad831409a73519b02b3047bb786aa125b6
Roy-Hillel <royhillel@gmail.com>
2024-02-02 12:29:51 +0200

    test2

--- database/types.py
+++ database/types.py
@@ -2,44 +2,35 @@ from bson import ObjectId
 
 from app.database import db
 from app.database.schedule import collection_schedules
+from app.models.models import Type
 
-collection_groups = db['groups']
-
-
-def add_group(group):
-    result = collection_groups.insert_one(group.dict())
-    return collection_schedules.update_one({"_id": ObjectId(group.schedule_id)},
-                                           {"$push": {"group_ids": ObjectId(result.inserted_id)}}), result.inserted_id
+collection_types = db['types']
+collection_shifts = db['shifts']
 
 
-def remove_group(group_id: str):
-    # remove group from schedule where schedule_id = result.schedule_id
-    group = collection_groups.find_one_and_delete({"_id": ObjectId(group_id)})
-    if group:
-        return collection_schedules.update_one({"_id": ObjectId(group['schedule_id'])},
-                                               {"$pull": {"group_ids": ObjectId(group_id)}})
-    return None
+def add_type(type):
+    return collection_types.insert_one(type.dict())
+
+
+def delete_type(type_id: str):
+    # remove type from shifts where type_id is in type_ids
+    type = collection_types.find_one_and_delete({"_id": ObjectId(type_id)})
+    if type:
+        collection_shifts.update_many({"type_id": type_id}, {"$pull": {"type_id": type_id}})
+    return type
 
 
 # handles both get all groups and get groups of schedule
-def get_groups(schedule_id: str):
-    if schedule_id:
-        groups = list(collection_groups.find({"schedule_id": schedule_id}))
-    else:
-        groups = list(collection_groups.find({}))
-    return [convert_unserializable(group) for group in groups]
+def update_type(type_id: str, type: Type):
+    return collection_types.update_one({"_id": ObjectId(type_id)}, {"$set": type.dict()})
 
 
-def get_group(group_id: str):
-    group = collection_groups.find_one({"_id": ObjectId(group_id)})
-    return convert_unserializable(group)
+def get_types():
+    types = list(collection_types.find({}))
+    for type in types:
+        type['_id'] = str(type['_id'])
+    return types
 
 
-# converts unserializable fields of group to str
-def convert_unserializable(group):
-    if group:
-        group['_id'] = str(group['_id'])
-        group['start_time'] = str(group['start_time'])
-        group['end_time'] = str(group['end_time'])
-        group['shifts_ids'] = [str(shift_id) for shift_id in group['shifts_ids']]
-    return group
+def get_type(type_id: str):
+    return collection_types.find_one({"_id": ObjectId(type_id)})
